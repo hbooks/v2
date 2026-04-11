e7e4739b3b7a242759c81deeceba21329b978fab
1806 <h00manpublisher@gmail.com>
2026-04-11 12:35:55 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import Cart from "./pages/Cart";
 import Membership from "./pages/Membership";
 import Updates from "./pages/Updates";
 import Profile from "./pages/profile";
+import ComingSoon from "./pages/ComingSoon";
 import PaymentSuccess from "./pages/PaymentSuccess";
 import Membershipsuccess from "./pages/Membershipsuccess";
 import Contact from "./pages/Contact";
@@ -75,6 +76,7 @@ if (location.pathname === "/unverified-shop") {
           <Route path="/member-shop" element={<MemberShop />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/coming-soon" element={<ComingSoon />} />
           <Route path="/membership" element={<Membership />} />
           <Route path="/updates" element={<Updates />} />
           <Route path="/payment-success" element={<PaymentSuccess />} />
